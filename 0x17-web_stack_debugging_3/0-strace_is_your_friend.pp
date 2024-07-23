12ce3d4ca7dade674300488c582c3ea3ec3e6de4
john karson <nuttall@nuttall.nuttall>
2024-07-23 09:51:14 +0300

    delete

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# fixing apache 500 error
-exec { 'fix err':
-onlyif  => 'test -e /var/www/html/wp-settings.php',
-command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-path    => '/usr/local/sbin/:/usr/local/bin:/usr/sbin/:/usr/bin:/sbin/:/bin'
-}
